5abdc4f7b0c8c25b1f5a9b64f02fd22b95a510e8
AnichoKraus <anichokraus3123@gmail.com>
2023-02-27 08:28:44 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-unused-vars': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id="app">
+      <p>React App</p>
+    </div>
+  )
+}
+
+export default App
